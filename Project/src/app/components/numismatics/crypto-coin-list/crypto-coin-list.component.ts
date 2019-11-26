da6fa2594bbd85888a65d0432e81bd931d1976ee
Lucian Toma <tomalucian11@gmail.com>
2019-11-27 00:10:50 +0200

    Display actual coin images on Numismatics page

--- Project/src/app/components/numismatics/crypto-coin-list/crypto-coin-list.component.ts
+++ Project/src/app/components/numismatics/crypto-coin-list/crypto-coin-list.component.ts
@@ -9,17 +9,17 @@ export class CryptoCoinListComponent implements OnInit {
   cryptoCoins = [
     {
       "name" : 'Ethereum',
-      "icon" : 'ethereum',
+      "icon" : 'ethereum-2-256.png',
       "description" : 'Ethereum is a global, decentralized platform for money and new kinds of applications.'
     },
     {
       "name" : 'BitCoin',
-      "icon" : 'bitcoin',
+      "icon" : 'bitcoin-256.png',
       "description" : 'Bitcoin is an innovative payment network and a new kind of money. Find all you need to know and get started with Bitcoin on bitcoin.org.'
     }
     ,{
       "name" : 'Ripple',
-      "icon" : 'shekel sign',
+      "icon" : 'ripple-2-256.png',
       "description" : 'Ripple enables banks, payment providers, digital asset exchanges and corporates to send money globally using advanced blockchain technology.'
     },
   ];
